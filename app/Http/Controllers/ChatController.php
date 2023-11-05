7f3fb5f8d8e360eae1c0a075bcb348efa8319591
Eduardo Henrique <125614516+dev-edufreitas@users.noreply.github.com>
2023-11-05 20:51:28 -0300

    feat: incrementChatEventAndPusher

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\Message;
+use Illuminate\Http\Request;
+
+
+class ChatController extends Controller
+{
+    public function message(Request $request)
+    {
+
+        event(new Message( $request->input('message'), $request->input('username')));
+
+    }
+}
